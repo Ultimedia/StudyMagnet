395fb24e07b04d72b39903c86dacbb54af459a67
Maarten Bressinck <maartenbressinck@gmail.com>
2013-04-11 13:47:18 +0100

    Added university field

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
 
 $(document).ready(function() {
+
+
+	/* Scroll Down Button */
 	$("#scrollDown").click(function() {
 	     $('html, body').animate({
 	         scrollTop: $(".content-light").offset().top
 	     }, 500);
 	 });
+
+
+	/* Subscribe to the mailing list */
+	$("#subscribe-button").click(function(){
+		// client side validation
+
+		// connect to a service / eg php and store data
+
+		// wait for callback and show the error or succes message and hide the form
+		$('#subscribe-form form').fadeOut(500,function(){
+			$('#succes-message').show().css("display","inline-block");;
+		});
+	});
+
+
 });
